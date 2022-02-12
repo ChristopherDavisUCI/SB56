0dcae8d6940c5cf31c9959decf6113123f55f78d
Christopher Davis <82750240+ChristopherDavisUCI@users.noreply.github.com>
2022-02-12 15:10:32 -0800

    Edit in browser

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import nfl_data_py as nfl
 
-st.title("I made this in the browser!")
+st.title("Super Bowl 56")
+
+df_schedule = nfl.import_schedules([2021])
+df_players = nfl.import_rosters([2021])
+df_pbp = nfl.import_pbp_data([2021]) #play-by-play
+
+st.write(df_pbp.head())
