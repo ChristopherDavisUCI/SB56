728b9e5f0965b375301d91dd125fd466c1d3d3ce
Christopher Davis <82750240+ChristopherDavisUCI@users.noreply.github.com>
2022-02-12 15:51:38 -0800

    st.table

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ st.write(f"The median longest reception for {wr} is {rec_series.median()} yards.
 
 st.write(f"Longest reception by week for {wr}:")
 
-st.write(rec_series)
+st.table(rec_series)
 
 st.write("Sorted list of longest receptions:")
 
